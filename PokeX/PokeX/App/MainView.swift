0b1e17c2da08dbcff19cb456bf5c4374b3c55bc3
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2024-04-18 14:28:11 +0300

    feat: created base project

---
+++ PokeX/PokeX/App/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeX
+//
+//  Created by MIKHAIL ZHACHKO on 18.04.24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ PokeX/PokeX/App/PokeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeXApp.swift
+//  PokeX
+//
+//  Created by MIKHAIL ZHACHKO on 18.04.24.
+//
+
+import SwiftUI
+
+@main
+struct PokeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
